e1689c87a810a3be6d8f21bd5decdd7332081a2c
Adidas177 <85192758+Adidas177@users.noreply.github.com>
2021-06-12 18:51:49 +0400

    Add files via upload

---
+++ Homework1.js
@@ -0,0 +1,61 @@
+//Davit jan nax minch lucumnerin ancnele asem vor xndirneri lucumnere edqan el hesht chein stacvum mots,xndirner uneca 2 ev 5 xndirneri het kapvac
+
+// xndir 1
+var x = 12
+var y = 4
+
+x=x+y
+y=x-y
+x=x-y
+
+//kstacvi x=4 y=12 
+
+//xndir 2
+let a = 367
+
+let lastDigit = a%10
+
+if (lastDigit>0) {
+    console.log(lastDigit*100+(a-a%10)/10);
+} else if (lastDigit===0) {
+    console.log(a);
+}
+
+//Davit jan,ays xndri vra shat em mtacel,bayc vorpes miak lucum sa ekav mtqis,knayenq a-i arzheqi tveri qanake (ayisnqn ete eranish e *100,qaranish *1000 ev ayln) banadzevi mej kpoxenq
+
+// xndir 3
+var num1 = 45
+var num2 = -12
+var num3 = 0
+var num4 = 3
+var num5 = -15
+
+var mid = (num1+num2+num3+num4+num5)/5
+
+mid
+//ardyunqum kstananq 5 tveri mijin arjeqe
+
+
+//xndir 4
+let a = 45
+let b = 26
+let c = 78
+
+if (a>b && b>c) {
+    console.log (c,b,a);
+} else if (a>c && b<c) {
+    console.log (b,c,a);
+} else if (a>b && a<c) {
+    console.log (b,a,c);
+} else if (a<b && a>c) {
+    console.log (c,a,b);
+} else if (a<b && b<c) {
+    console.log (a,b,c);
+} else if (a<b && b>c) {
+    console.log (a,c,b);
+}
+
+//ardyunqum enteric heto cankacac a,b,c arjeqneri cankacac popoxutyan depqum kstananq tvere achman kargov
+
+//xndir 5
+//Chem haskanum xndri pahanje,chem karoxanum:(
